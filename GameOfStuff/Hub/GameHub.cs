aa5bb824aa410b4e15aae6d3e74f7fb5cb603740
Diego Rodrigues <Diegophotography@gmail.com>
2019-11-24 18:41:22 -0500

    initial commit, signalR comms

---
+++ GameOfStuff/Hub/GameHub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace GameOfStuff
+{
+    public class GameHub : Hub
+    {
+        public void SubmitAnswer(GameModel gameModel)
+        {
+            Clients.Group(gameModel.SessionID).SendAsync("AddAnswer", gameModel.Answer);
+        }
+
+        public void JoinGroup(string groupName)
+        {
+            Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+        }
+    }
+
+    public class GameModel
+    {
+        public string SessionID { get; set; }
+        public string Answer { get; set; }
+    }
+}
